279ec8d5565b141a0f0ea1cf8b00780a8b81c521
cleisonoliveira49-afk <cleisonoliveira49@gmail.com>
2025-08-06 16:36:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("8254941787:AAFvYg8HK69leAkv5RmR_WockW2H1Ee8mWw")
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Fala, piloto! TARS tá online.")
